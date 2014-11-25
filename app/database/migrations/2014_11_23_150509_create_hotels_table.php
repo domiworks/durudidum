4841dbcb0d988b10fc365e29570a609042eaa5c7
david senjaya <davidsenjayagm@gmail.com>
2014-11-25 18:07:53 +0700

    no message

--- app/database/migrations/2014_11_23_150509_create_hotels_table.php
+++ app/database/migrations/2014_11_23_150509_create_hotels_table.php
@@ -18,20 +18,19 @@ class CreateHotelsTable extends Migration {
 			$table->increments('id');
 			$table->string('nama');
 			$table->string('alamat');
-			$table->string('tipe_kamar');
 			$table->string('jumlah_bintang');
 			$table->time('waktu_check_in');
 			$table->time('waktu_check_out');
 			$table->string('folder_gallery_path');
-			$table->decimal('harga_per_kamar_per_malam', 11,2);
 			$table->longText('keterangan');
 			$table->longText('kebijakan_pembatalan');
 			$table->integer('kapasitas_kamar');
 			$table->integer('sisa_kamar');
-			$table->string('fasilitas')->nullable();
+			$table->string('fasilitas_dasar')->nullable();
+			$table->string('fasilitas_lain')->nullable();
 			$table->integer('id_area')->unsigned();
-			$table->longText('review_terbaik')->unsigned();
-			$table->longText('maps_path')->unsigned();
+			$table->longText('review_terbaik');
+			$table->longText('maps_path');
 			//pisahin hotel detail
 			
 			$table->timestamps();
