9030816de3451740aedc74a43b1c83299a010830
Christopher Andrian <christandrian@gmail.com>
2014-11-26 23:44:46 +0700

    no message

---
+++ app/views/pages/front_end/payment/payment_cancel.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.front_end.book_layout')
+@section('content')
+
+
+<section>
+    <div class="container">
+        <div class="row" style="padding-top:20px;">
+
+            cancel
+        </div>
+
+</section>
+@stop
---
+++ app/views/pages/front_end/payment/payment_order.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.front_end.book_layout')
+@section('content')
+
+
+<section>
+    <div class="container">
+        <div class="row" style="padding-top:20px;">
+
+		berhasil
+        </div>
+
+   
+   
+</section>
+@stop
